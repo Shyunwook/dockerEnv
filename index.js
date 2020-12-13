de11ac632c5ac18ced1321d6c4bfae968b91058e
Shyunwook <36650417+Shyunwook@users.noreply.github.com>
2020-12-13 17:28:01 +0900

    xterm connected remote env

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const child_process = require("child_process");
+// const child_process = require("child_process");
 
-app.get("/test", async (req, res) => {
-  // let process = await child_process.execSync(`docker run -it --name py -d python:3.7`);
-  let process = await child_process.execSync(`echo '10+20' | script -q $(pwd)/stdin docker attach py`);
-  // child_process.spawn(process).stdin.write("docker attac");
-  res.send(process.toString());
+// app.get("/test", async (req, res) => {
+//   // let process = await child_process.execSync(`docker run -it --name py -d python:3.7`);
+//   let process = await child_process.execSync(`echo '10+20' | script -q $(pwd)/stdin docker attach py`);
+//   // child_process.spawn(process).stdin.write("docker attac");
+//   res.send(process.toString());
+// });
+
+// app.listen(5050, () => {
+//   console.log("listening!!!");
+// });
+
+const Terminal = require("./terminal.class.js").Terminal;
+let terminalServer = new Terminal({
+  role: "server",
+  shell: process.platform === "win32" ? "cmd.exe" : "bash",
+  port: 3000,
 });
 
-app.listen(5050, () => {
-  console.log("listening!!!");
-});
+terminalServer.onclosed = (code, signal) => {
+  console.log("Terminal closed - " + code + ", " + signal);
+  app.quit();
+};
+terminalServer.onopened = () => {
+  console.log("Connected to remote");
+};
+terminalServer.onresized = (cols, rows) => {
+  console.log("Resized terminal to " + cols + "x" + rows);
+};
+terminalServer.ondisconnected = () => {
+  console.log("Remote disconnected");
+};
